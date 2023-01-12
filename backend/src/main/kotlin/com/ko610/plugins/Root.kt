160b14a986ce812f24928191905ae006cd8dcd00
yuuki1293 <yuuki1293@outlook.jp>
2023-01-12 10:47:22 +0900

    feat(backend): プラグインの位置を移動

--- backend/src/main/kotlin/com/ko610/plugins/Root.kt
+++ backend/src/main/kotlin/com/ko610/plugins/Root.kt
@@ -1,8 +1,18 @@
 package com.ko610.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
+
     userRouting()
     loginRouting()
 }
--- backend/src/main/kotlin/com/ko610/plugins/User.kt
+++ backend/src/main/kotlin/com/ko610/plugins/User.kt
@@ -3,30 +3,18 @@ package com.ko610.plugins
 import com.ko610.models.Setting
 import com.ko610.models.User
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.lang.NumberFormatException
 import java.time.LocalDate
 
 fun Application.userRouting() {
-    install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            isLenient = true
-        })
-    }
-
-    install(Routing)
-
     routing {
         post("/user") {
             try {
