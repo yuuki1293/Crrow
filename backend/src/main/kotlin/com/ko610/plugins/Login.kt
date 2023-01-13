32073a207f63cad6e78a756bf1335ff2a060c8fa
yuuki1293 <yuuki1293@outlook.jp>
2023-01-13 10:29:30 +0900

    feat(backend): update login

--- backend/src/main/kotlin/com/ko610/plugins/Login.kt
+++ backend/src/main/kotlin/com/ko610/plugins/Login.kt
@@ -1,48 +1,86 @@
 package com.ko610.plugins
 
-import io.ktor.client.*
+import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
-import io.ktor.client.engine.cio.*
+import kotlinx.serialization.Serializable
+import java.security.KeyFactory
+import java.security.interfaces.ECPrivateKey
+import java.security.interfaces.ECPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 fun Application.loginRouting() {
-    install(Sessions) {
-        cookie<UserSession>("user_session")
-    }
+    val privateKeyString = environment.config.property("jwt.privateKey").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+    val jwkProvider = JwkProviderBuilder(issuer)
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
 
     install(Authentication) {
-        oauth("auth-oauth") {
-            urlProvider = { "http://localhost:8081/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "microsoft",
-                    authorizeUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-                    accessTokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("MICROSOFT_CLIENT_ID"),
-                    clientSecret = System.getenv("MICROSOFT_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://graph.microsoft.com/User.Read"),
-                )
+        jwt("auth-jwt") {
+            realm = myRealm
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
             }
-            client = HttpClient(CIO)
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
         }
     }
 
     routing {
-        authenticate("auth-oauth") {
-            get("/login") {}
-
-            get("/callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondText(principal?.accessToken.toString())
+        post("/login") {
+            val user = call.receive<Login>()
+            // TODO: ここでDBと照合する
+            val publicKey = jwkProvider.get("2f85532c-aaeb-4f74-959b-49143ab1333c").publicKey
+            val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyString))
+            val privateKey = KeyFactory.getInstance("EC").generatePrivate(keySpecPKCS8)
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.name)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.ECDSA256(publicKey as ECPublicKey, privateKey as ECPrivateKey))
+            call.respond(hashMapOf("token" to token))
+        }
+        get("/.well-known/jwks.json") {
+            val file = this.javaClass
+                .classLoader
+                .getResourceAsStream(".well-known/jwks.json")
+                ?.bufferedReader()
+                ?.use { it.readText() }
+            if(!file.isNullOrEmpty()){
+                call.respondText(file)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        authenticate("auth-jwt") {
+            get("/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
             }
         }
     }
 }
 
-data class UserSession(val accessToken: String)
+@Serializable
+data class Login(val name: String, val password: String)
