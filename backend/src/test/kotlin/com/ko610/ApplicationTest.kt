6533d1edb563e972e3e5a880ee501cca48fe2747
yuuki1293 <yuuki1293@outlook.jp>
2023-01-13 10:44:59 +0900

    test(backend): login

--- backend/src/test/kotlin/com/ko610/ApplicationTest.kt
+++ backend/src/test/kotlin/com/ko610/ApplicationTest.kt
@@ -1,13 +1,39 @@
 package com.ko610
 
-import io.ktor.server.routing.*
+import com.ko610.plugins.Login
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import kotlin.test.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
-import com.ko610.plugins.*
+import kotlinx.serialization.*
+import kotlin.test.*
 
 class ApplicationTest {
+    @Test
+    fun testLogin() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val responseLogin = client.post("/login"){
+            contentType(ContentType.Application.Json)
+            setBody(Login("hoge", "fuga"))
+        }
+        assertEquals(HttpStatusCode.OK, responseLogin.status)
+
+        @Serializable
+        data class Token(val token: String)
+        val body: Token = responseLogin.body()
+
+        val responseHello = client.get("/hello"){
+            bearerAuth(body.token)
+        }
+        assertEquals(HttpStatusCode.OK, responseHello.status)
+        println(responseHello.bodyAsText())
+    }
 }
